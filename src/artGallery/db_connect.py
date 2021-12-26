a8fbaf9ce71561dc1535c8af3b22c11d0ea5a77c
OmarAly23 <aly.omar@cs.ku.edu.kw>
2021-12-26 05:03:32 -0500

    python file for the database connection
    import db_connect and variables inside to connect to the database from different modules

---
+++ src/artGallery/db_connect.py
@@ -0,0 +1,5 @@
+from pymongo import MongoClient
+
+connectToMongo = "mongodb+srv://Zach-den:artGallery%40cs470@cluster0.briba.mongodb.net/artGallery?retryWrites=true&w=majority"
+client = MongoClient(connectToMongo)
+dbname = client['artGallery']
