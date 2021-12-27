34de6b87722a31ddcaabc97a9a643721d3c5f1a3
OmarAly23 <aly.omar@cs.ku.edu.kw>
2021-12-26 22:12:41 -0500

    Signing up adds a new user

--- src/artGallery/art/views.py
+++ src/artGallery/art/views.py
@@ -1,21 +1,40 @@
-from django.shortcuts import render
-from django.http import HttpResponse
 from pymongo import MongoClient
 
-# from ..db_connect import dbname
-# collection_name = dbname['User']
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# import sys
+# sys.path.append('..')
+# sys.path.append('.')
+
+
+connectToMongo = "mongodb+srv://Zach-den:artGallery%40cs470@cluster0.briba.mongodb.net/artGallery?retryWrites=true&w=majority"
+client = MongoClient(connectToMongo)
+dbname = client['artGallery']
+collection_name = dbname['User']
+
 
 # Create your views here.
 
 def index(request):
     return render(request, '../templates/index2.html')
 
+
 # should take in the values
 def sign_in(request):
-
     return render(request, '../templates/sign_in.html')
 
+
 def sign_up(request):
+    if request.method == 'POST':
+        em = request.POST['email']
+        passw = request.POST['password']
+        user_to_be_added = {
+            'email': em,
+            'password': passw
+        }
+        collection_name.insert_one(user_to_be_added)
+        print('User has been added!')
     return render(request, '../templates/sign_up.html')
 # Create a function for handling users
 
