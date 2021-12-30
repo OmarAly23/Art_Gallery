3a12cb0df0aa379e5445caf05f3231ed7971ee01
OmarAly23 <aly.omar@cs.ku.edu.kw>
2021-12-30 08:29:34 -0500

    Added sessions urls

--- src/artGallery/art/urls.py
+++ src/artGallery/art/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('signin', views.sign_in, name='sign_in'),
-    path('signup', views.sign_up, name='sign_up')
-    # path('signup/', views.signup_view, name='signup'),
+    path('signup', views.sign_up, name='sign_up'),
+    path('testcookie/', views.cookie_session),
+    path('deletecookie/', views.cookie_delete),
 ]
