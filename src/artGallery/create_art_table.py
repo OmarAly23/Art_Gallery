125cdbf15ee756414f1ebf138dcc815cbca95ab7
OmarAly23 <aly.omar@cs.ku.edu.kw>
2021-12-30 06:48:15 -0500

    Art table created: Inserted images and read them back from the DB

--- src/artGallery/create_art_table.py
+++ src/artGallery/create_art_table.py
@@ -1,22 +1,39 @@
 from db_connect import dbname
+# libraries to help with image storage
+from PIL import Image
+import io
+from bson.binary import Binary
+import matplotlib.pyplot as plt
 
 collection_name = dbname['art']
 
-art1 = {
-    "artist_id": "R1",
-    "art_title": "",
-    "art_document": "",
-    "artist": "Nero n sei",
-}
 
-art2 = {
-    "artist_id": "R2",
-    "art_title": "",
-    "art_document": "",
-    "artist": "Gabr Jurk",
-}
+im = Image.open('./art/static/admin/img/Berserk_Manga.jpeg')
+image_bytes = io.BytesIO()
+im.save(image_bytes, format='JPEG')
+# image variable contains the actual byte representation of the image
+image = image_bytes.getvalue()
 
-collection_name.insert_many([art1, art2])
+im1 = Image.open('./art/static/admin/img/Sequence.jpeg')
+image_bytes1 = io.BytesIO()
+im1.save(image_bytes1, format="JPEG")
+image1 = image_bytes1.getvalue()
+
+# art1 = {
+#     "artist_id": "R1",
+#     "art_title": "Berserk Manga",
+#     "art_document": image,
+#     "artist": "Nero n sei",
+# }
+#
+# art2 = {
+#     "artist_id": "R2",
+#     "art_title": "Sequence",
+#     "art_document": image1,
+#     "artist": "Gabr Jurk",
+# }
+#
+# collection_name.insert_many([art1, art2])
 
 count = collection_name.count()
 print(count)
@@ -24,5 +41,7 @@ print(count)
 result = collection_name.find({})
 
 for element in result:
-    print(element)
+    plt_img = Image.open(io.BytesIO(element['art_document']))
+    plt.imshow(plt_img)
+    plt.show()
 
