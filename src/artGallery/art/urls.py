c42c5e7a6a042f612813733c3f8d0409bfe7d884
OmarAly23 <aly.omar@cs.ku.edu.kw>
2021-12-31 05:33:06 -0500

    Modification in urls

--- src/artGallery/art/urls.py
+++ src/artGallery/art/urls.py
@@ -4,10 +4,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('login', views.sign_in, name='sign_in'),
-    path('sign_up', views.sign_up, name='sign_up'),
-    path('testcookie/', views.cookie_session),
-    path('deletecookie/', views.cookie_delete),
-    path('create/', views.create_session),
+    path('login/', views.sign_in, name='sign_in'),
+    path('sign_up/', views.sign_up, name='sign_up'),
+    path('logout/', views.logout, name='logout'),
+    # path('testcookie/', views.cookie_session),
+    # path('deletecookie/', views.cookie_delete),
+    # path('create/', views.create_session),
     # path('access/', views.access_session),
 ]
