580684a95ac263f0e807f07d1c5939f384a43223
OmarAly23 <aly.omar@cs.ku.edu.kw>
2022-01-12 21:30:06 +0300

    Convetrs binaries to jpeg

--- src/artGallery/art/templatetags/home_extras.py
+++ src/artGallery/art/templatetags/home_extras.py
@@ -1,6 +1,8 @@
 from django import template
 from base64 import b64encode
-
+import base64
+from PIL import Image
+from io import BytesIO
 
 register = template.Library()
 
@@ -8,3 +10,10 @@ register = template.Library()
 def bin_2_img(_bin):
     if _bin is not None:
         return b64encode(_bin).decode('utf-8')
+
+@register.filter(name='bin2jpg')
+def bin2jpg(_bin):
+    if _bin is not None:
+        im = Image.open(BytesIO(bin_2_img(_bin)))
+        im.save('accept.jpg', 'JPEG')
+        return im
